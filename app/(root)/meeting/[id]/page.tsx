0246a66d5186ed555157f2ecc7726517789796a1
ShrimpDev <muhammadfaheemwahid@gmail.com>
2024-09-30 11:24:01 +0800

    finihing yoom clone

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+"use client";
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+
+import { useState } from "react";
 
 type Props = {
   params: {
@@ -7,7 +15,25 @@ type Props = {
 };
 
 const page = ({ params }: Props) => {
-  return <div>Meeting room #{params.id}</div>;
+  const { user, isLoaded } = useUser();
+  const { call, isCallLoading } = useGetCallById(params.id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default page;
